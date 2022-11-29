52051423da0d18ca82400a0d0ed0a0ffb4df5b29
Jamie Donnelly <jamie.donnelly01@gmail.com>
2022-11-29 10:43:00 +0000

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,14 +2,20 @@ import torch
 from torch import nn
 from losses import RMSELoss
 
+import torch 
+from torch import nn
+from losses import RMSELoss
+
 class EarlyStopping():
     
-    def __init__(self,metric,tolerance,patience):
+    def __init__(self,model,metric,tolerance,patience):
+        self.model = model
         self.metric = metric
         self.tolerance = tolerance
         self.patience = patience
         self._scores = torch.tensor([])
         self._loss = self._metric_check()
+        self.model_states = []
         
     def _metric_check(self):
         if self.metric == "mse":
@@ -40,9 +46,21 @@ class EarlyStopping():
         score = self._loss(predictions,ytest)
         self._scores = torch.hstack([self._scores,score])
         print(f"Test {len(self._scores)+1} Evaluation Score: {score.item():.4f}")
-            
+
+    def _store_state(self,model):
+        self.model_states.append(model.state_dict())
+
+    def best_model(self):
+        # This is currently very poor in terms of memory management since it holds potentially large numbers of model weights in memory.
+        # A future implementation will work by saving model states to a specified directory then deleting all
+        # state dicts once the best model is returned 
+        lowest_score_index = torch.argmin(self._scores)
+        print(f"Returning model state from iteration")
+        return self.model.load_state_dict(self.model_states[lowest_score_index])
+
     def __call__(self,model,Xtest,ytest):
         self._evaluate_predictions(model,Xtest,ytest)
         if self._scores.shape[0] > self.patience:
             if self._evaluate_convergence() or self._evaluate_increase():
                 raise ValueError
+
